6b188c8d64b71b9839f1cf2f7dfb19c0eecbb9eb
ASteinheiser <asteinhe@asu.edu>
2017-04-25 11:57:04 -0700

    /recognition works, updated readme

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Drewaston
 
 This is a simple micro-service that will perform image recognition using IBM Watson.
+
+### Usage
+Clone, install, run:
+```
+git clone https://github.com/ASteinheiser/drewatson.git
+npm install
+npm start
+```
+In another terminal tab:
+```
+curl -X POST -F "image=@bloom.jpeg" localhost:3000/recognition
+```
--- index.js
+++ index.js
@@ -1,38 +1,40 @@
 var express = require('express')
-var package = require('./package')
-
-var fs = require('fs')
+var fileUpload = require('express-fileupload')
 var VisualRecognitionV3 = require('watson-developer-cloud').VisualRecognitionV3
+
+var package = require('./package')
 var credentials = require('./watson.json')
 
 var visual_recognition = new VisualRecognitionV3({
   api_key: credentials.api_key,
   version_date: '2016-05-19'
-});
+})
 
 var app = express()
-
-app.set('port', process.env.PORT || 3000);
-
-app.get('/', function(request, response) {
-  var params = {
-    images_file: fs.createReadStream('./bloom.jpeg'),
-    classifier_ids: ["hue_1859332055", "default"]
-  };
-
-  visual_recognition.classify(params, function(err, res) {
-    if (err) return response.send(err)
-    response.send(JSON.stringify(res, null, 2))
-  });
-})
+app.use(fileUpload())
+app.set('port', process.env.PORT || 3000)
 
 app.get('/version', function(request, response) {
   response.send(package.version)
 })
 
 app.listen(app.get('port'), () => {
- console.log('App is running at http://localhost:%d in %s mode', app.get('port'), app.get('env')); 
- console.log('  Press CTRL-C to stop\n');
-});
+ console.log('App is running at http://localhost:%d in %s mode', app.get('port'), app.get('env'))
+ console.log('Press CTRL-C to stop\n')
+})
+
+app.post('/recognition', function(request, response) {
+  if (!request.files) return response.status(400).send('No files were uploaded...')
+
+  var params = {
+    images_file: request.files.image.data,
+    classifier_ids: ["hue_1859332055", "default"]
+  }
+
+  visual_recognition.classify(params, function(err, res) {
+    if (err) return response.send(err)
+    response.send(JSON.stringify(res, null, 2))
+  })
+})
 
 module.exports = app
